e62e5db5a8088ec0492cc1a7ae54821393a3b609
Shakil Ahmmed <30585539+ShakilAhmmed@users.noreply.github.com>
2019-08-02 22:13:42 +0600

    Update 16.py

--- Set_1/16.py
+++ Set_1/16.py
@@ -1,5 +1,5 @@
 fruits_name = input()
-if fruits_name.casefold().startswith(('a', 'e', 'i', 'o', 'u')):
+if fruits_name.casefold().startswith(('a', 'e', 'i', 'o', 'u','A','E','I','O','U')):
     print(f"This is an {fruits_name}")
 else:
     print(f"This is a {fruits_name}")
