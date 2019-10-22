902015c0a1198231e657cf1d0a1bdfdf7a5651d2
Shakil Ahmmed <shakilfci461@gmail.com>
2019-10-22 09:54:37 +0600

    New Update

---
+++ Set_1/one_sol.py
@@ -0,0 +1,3 @@
+n = int(input())
+numbers = [x for x in range(0, n)]
+print(numbers)
---
+++ Turtle/Line.py
@@ -0,0 +1,11 @@
+import turtle
+
+my_window = turtle.Screen()
+my_window.delay(100)
+my_window.bgcolor("#74b9ff")
+my_pen = turtle.Turtle()
+my_pen.forward(150)
+my_pen.left(90)
+my_pen.forward(120)
+my_pen.color("red")
+my_pen.pensize(12)
